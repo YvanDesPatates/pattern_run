4240baa7467aa7f92db8698dd042465184d7500e
yvan <yvis99@gmail.com>
2025-08-07 13:15:33 +0200

    feat(gameover): gameover screen with fade in

---
+++ Assets/Scripts/GameOver/GameOverCanvasController.cs
@@ -0,0 +1,47 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverCanvasController : MonoBehaviour
+{
+    [SerializeField] private int backgroundAlphaTarget = 200;
+    [SerializeField] private float fadeDuration = 1.0f;
+    [SerializeField] private TextMeshProUGUI gameOverText;
+    [SerializeField] private Image backgroundImage;
+
+    private void Awake()
+    {
+        SetTextAlpha(0);
+        SetBackgroundAlpha(0);
+    }
+
+    void Update()
+    {
+        // fade in the Game Over text and background
+        if (gameOverText.color.a < 1.0f)
+        {
+            float alpha = Mathf.Min(gameOverText.color.a + (Time.deltaTime / fadeDuration), 1.0f);
+            SetTextAlpha(alpha);
+        }
+
+        if (backgroundImage.color.a < backgroundAlphaTarget / 255f)
+        {
+            float alpha = Mathf.Min(backgroundImage.color.a + (Time.deltaTime / fadeDuration), backgroundAlphaTarget / 255f);
+            SetBackgroundAlpha(alpha);
+        }
+    }
+    
+    private void SetTextAlpha(float alpha)
+    {
+        Color textColor = gameOverText.color;
+        textColor.a = alpha;
+        gameOverText.color = textColor;
+    }
+    
+    private void SetBackgroundAlpha(float alpha)
+    {
+        Color bgColor = backgroundImage.color;
+        bgColor.a = alpha;
+        backgroundImage.color = bgColor;
+    }
+}
