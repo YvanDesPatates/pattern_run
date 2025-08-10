4596b49128b2340ead49fc9a7051f84e042869d8
yvan <yvis99@gmail.com>
2025-08-10 15:59:23 +0200

    feat(jump): enhance jump feeling

--- Assets/Scripts/PlayerMovements/JumpStrategy.cs
+++ Assets/Scripts/PlayerMovements/JumpStrategy.cs
@@ -7,6 +7,8 @@ public class JumpStrategy: IMovementStrategy
 
     private static readonly Vector3 InitialGravity = Physics.gravity;
     
+    private bool _keyIsBeingPressed = false;
+    
     public JumpStrategy()
     {
         Physics.gravity = InitialGravity * GravityModifier;
@@ -14,16 +16,23 @@ public class JumpStrategy: IMovementStrategy
 
     public void OnActionKeyPressed(PlayerController player)
     {
-        if ( ! player.IsOnGround) return;
+        _keyIsBeingPressed = true;
+    }
+
+    public void OnActionKeyReleased(PlayerController player)
+    {
+        _keyIsBeingPressed = false;
+    }
+
+    public void Update(PlayerController player)
+    {
+        if ( !_keyIsBeingPressed || ! player.IsOnGround) return;
         
+        player.IsOnGround = false;
         player.PlayerRb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
         player.SetAnimationTrigger("Jump_trig"); 
         player.PlayJumpSound();
     }
-
-    public void OnActionKeyReleased(PlayerController player){}
-    
-    public void Update(PlayerController player){}
     
     public void ResetBeforeDestroy()
     {
